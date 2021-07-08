b106aa2ac5ee14ed13f887d028abfa10bd2ee06c
Denis Alexandre da Silva <denisinfo@alumni.usp.br>
2021-07-08 12:37:39 -0300

    2.3  Comparando variáveis

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,15 @@
-print("Bem vindo no jogo de Adivinhação")
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+if (numero_secreto == chute):
+    print("Voce acertou")
+else:
+    print("Voce erro")
