eb29f54c757a19287dc3552143e31cc248e19b35
Denis Alexandre da Silva <denisinfo@alumni.usp.br>
2021-07-12 11:41:37 -0300

    8.6 Diferenciando um arquivo executado de um importado

--- jogos/jogos.py
+++ jogos/jogos.py
@@ -1,17 +1,23 @@
 import forca
-import  adivinhacao
+import adivinhacao
 
-print("*********************************")
-print("***Escolha o seu jogo!***")
-print("*********************************")
 
-print("(1) Forca (2) Adivinhacao")
+def escolhe_jogos():
+    print("*********************************")
+    print("***Escolha o seu jogo!***")
+    print("*********************************")
 
-jogo = int(input("Qual jogo? "))
+    print("(1) Forca (2) Adivinhacao")
 
-if (jogo == 1):
-    print("Jogando forca")
-    forca.jogar()
-elif (jogo == 2):
-    print("Jogando adivinhacao")
-    adivinhacao.jogar()
+    jogo = int(input("Qual jogo? "))
+
+    if (jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif (jogo == 2):
+        print("Jogando adivinhacao")
+        adivinhacao.jogar()
+
+
+if (__name__ == "main"):
+    jogar()
